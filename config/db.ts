13f5ff3ee558a3c6707557b7caf3f07e1bd30097
tim robillard <hello@timrobillard.ca>
2021-04-01 22:27:50 -0400

    getting used to setup

--- config/db.ts
+++ config/db.ts
@@ -1,13 +1,12 @@
 import pg from 'pg';
-import logging from './logging';
 const Pool = pg.Pool;
-logging.debug('db_config', process.env.POSTGRES_PASSWORD!)
+
 let pool = new Pool({
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
     port: 5432,
-    host: 'vultr_db_1',
+    host: process.env.POSTGRES_HOST,
 })
 
 export default pool;
