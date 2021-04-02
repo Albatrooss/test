a74a3ea05927cd70a6aea2e2e7875f8a3369b6f4
tim robillard <hello@timrobillard.ca>
2021-04-01 22:17:10 -0400

    added dbConfig and volumes

---
+++ config/dbConfig.sql
@@ -0,0 +1,6 @@
+CREATE TABLE todo(
+    todo_id SERIAL PRIMARY KEY,
+    text VARCHAR(140),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
